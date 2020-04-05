6bcefb4d8df7df07deabdfc82fc04a4ab7268c04
brentkulwicki <brentkulwicki@gmail.com>
2020-04-05 16:47:48 -0400

    fixed the vanilla basic demo

--- vanilla-js-jquery/basic-example/src/index.js
+++ vanilla-js-jquery/basic-example/src/index.js
@@ -1,33 +1,19 @@
 import FlatfileImporter from "flatfile-csv-importer";
 import $ from "jquery";
-import { makeFloat } from "./functions";
-import { translations } from "./translations";
+import {flatfileConfig} from "./flatfile-config"
 
 FlatfileImporter.setVersion(2);
 
+const LICENSE_KEY = urlParams.get('license');
+// const LICENSE_KEY = "4171f0b4-5f5c-4b32-a008-356ebb813e4e"
+
+if (!LICENSE_KEY) {
+    let github = document.getElementById('githubLogin')
+    github.style.display = "none";
+}
+
 const importer = new FlatfileImporter(
-    "4171f0b4-5f5c-4b32-a008-356ebb813e4e", // Put your license key in to show imports on your client dashboard
-    {
-        fields: [
-            {
-                key: "code",
-                alternates: ["sku", "productcode", "produktnummer", "artikelnummer"],
-                label: "Artikelnummer", // EN - Article number
-                description: "Der Code unter dem das Produkt im System geführt wird", // EN - The code under which the product is listed in the system
-                validators: [{ validate: "required" }]
-            }
-        ],
-        type: "Products",
-        allowInvalidSubmit: false, // being set to false, the end user has to fix all the issues for the data before submitting to you all
-        managed: true, // this gives you access to see impoorts on our dashboard
-        allowCustom: true, // this lets your users upload a custom column that you haven't accounted for in the above fields
-        disableManualInput: false, // this would let someone manually input the data instead of uploading a file
-        i18nOverrides: {
-            "en-US": translations,
-            de: translations,
-            en: translations
-        }
-    }
+    LICENSE_KEY, flatfileConfig
 );
 
 $("#launch").click(function() {
