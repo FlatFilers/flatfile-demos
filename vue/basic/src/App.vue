9b1285601ee93ccc30c65143906c61628e4e5cf6
brentkulwicki <brentkulwicki@gmail.com>
2020-10-22 10:23:59 -0400

    pushing changes

---
+++ vue/basic/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Flatfile msg="Flatfile + Vue"/>
+  </div>
+</template>
+
+<script>
+import Flatfile from "./components/Flatfile.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Flatfile
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
